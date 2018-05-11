2b75e2b1aba7c705493a4924312e7429efdcf344
hackerhgl <hackerhgl@gmail.com>
2018-05-11 19:40:29 +0500

    park

---
+++ lib/pages/ShoesConcept/ShoeTheme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ShoeTheme {
+  static TextStyle headerHeading() => new TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.w600,
+        fontSize: 22.0,
+      );
+
+  static TextStyle headerDescription() => new TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.w400,
+        fontSize: 15.0,
+        height: 1.06,
+      );
+}
--- lib/pages/ShoesConcept/main.dart
+++ lib/pages/ShoesConcept/main.dart
@@ -1,34 +1,56 @@
 import 'package:flutter/material.dart';
 
+import 'HeaderColumn.dart';
+
 class ShoesConcept extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
       body: new Content(
-        gradientColors: [
-          Colors.red,
-          Colors.purple,
-        ],
-        assetLink: "assets/Asics_Logo_1.png",
-      ),
+          gradientColors: [
+            Colors.red,
+            Colors.purple,
+          ],
+          logoLink: "assets/Asics_Logo_1.png",
+          headerHeading: 'GEL KAYANO TRAINER',
+          headerDescription:
+              'This entry-level model features a full-length Gum Rubber Outsole for excellent traction on the court. Rearfoot GEL Cushioning System provides enhanced cushioning - while the combination synthetic leather and mesh upper provides breathability and comfort. A great value for the multi-court player.',
+          contentHeading: "ASICS GEL KAYANO"),
     );
   }
 }
 
 class Content extends StatelessWidget {
-  Content({this.gradientColors: const <Color>[], this.assetLink});
+  Content({
+    this.gradientColors: const <Color>[],
+    this.logoLink,
+    this.headerHeading,
+    this.headerDescription,
+    this.contentHeading,
+  });
   final List<Color> gradientColors;
-  final String assetLink;
+  final String logoLink;
+  final String headerHeading;
+  final String headerDescription;
+  final String contentHeading;
+
+  final double _contnetHeight = 220.0;
 
   @override
   Widget build(BuildContext context) {
+    double _headerHeight = MediaQuery.of(context).size.height - _contnetHeight;
     // TODO: implement build
-    return new Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+    HeaderColumn column = new HeaderColumn(
+      headerDescription: this.headerDescription,
+      headerHeading: this.headerHeading,
+      logoLink: this.logoLink,
+    );
+    return new Stack(
+      fit: StackFit.expand,
       children: <Widget>[
-        new Expanded(
-          flex: 2,
+        new Container(
+          height: _headerHeight,
           child: new Container(
             decoration: new BoxDecoration(
               gradient: new LinearGradient(
@@ -36,24 +58,67 @@ class Content extends StatelessWidget {
                 colors: this.gradientColors,
               ),
             ),
-            child: new Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                new Container(
-                  margin: const EdgeInsets.only(top: 48.0, left: 32.0),
-                  child: new Image(
-                    image: new AssetImage(assetLink),
-                    width: 140.0,
+            child: column,
+          ),
+        ),
+        new Container(
+          margin: new EdgeInsets.only(top: _headerHeight),
+          color: Colors.red.withOpacity(1.0),
+          height: _contnetHeight,
+          child: new Column(
+            children: <Widget>[
+              new Row(
+                children: <Widget>[
+                  new Column(
+                    children: <Widget>[
+                      new Text(this.contentHeading),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+        new Positioned(
+          top: 250.0,
+          left: 36.0,
+          right: 36.0,
+          child: new Container(
+            child: new Transform(
+              alignment: Alignment.center,
+              // transform: new Matrix4.rotationZ(0.0),
+              transform: new Matrix4.rotationZ(0.39)..leftTranslate(16.0, 40.0),
+              child: new Stack(
+                alignment: Alignment.bottomCenter,
+                children: <Widget>[
+                  new Container(
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 4.0,
+                    ),
+                    child: null,
+                    height: 16.0,
+                    decoration: new BoxDecoration(
+                      borderRadius: const BorderRadius.all(
+                        const Radius.circular(16.0),
+                      ),
+                      // color: Colors.transparent,
+                      boxShadow: <BoxShadow>[
+                        new BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 32.0,
+                          offset: new Offset(0.0, 4.0),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                new Container(
-                  child: new Text("This is Shoe 1"),
-                )
-              ],
+                  new Image(
+                    image: new AssetImage('assets/shoes/yellow.png'),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
-        new Text('sad'),
       ],
     );
   }
